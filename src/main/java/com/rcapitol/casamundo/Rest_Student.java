30784ff38b26d34292efe9586d04f442dc99f41e
Grenne Reis <grenneglr@gmail.com>
2016-03-30 18:56:31 -0300

    criando classes java

---
+++ src/main/java/com/rcapitol/casamundo/Rest_Student.java
@@ -0,0 +1,64 @@
+package com.rcapitol.casamundo;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.types.ObjectId;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.rcapitol.casamundo.Student.Documento;
+import com.google.gson.Gson;
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+@Singleton
+// @Lock(LockType.READ)
+@Path("/student")
+
+public class Rest_Student {
+
+	@Path("/obter")	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONObject ObterStudent(@QueryParam("email") String email) throws UnknownHostException, MongoException {
+		Mongo mongo = new Mongo();
+		DB db = (DB) mongo.getDB("documento");
+		DBCollection collection = db.getCollection("diagrams");
+		BasicDBObject searchQuery = new BasicDBObject("documento.email", email);
+		DBObject cursor = collection.findOne(searchQuery);
+		JSONObject documento = new JSONObject();
+		BasicDBObject obj = (BasicDBObject) cursor.get("documento");
+		documento.put("documento", obj);
+		mongo.close();
+		return documento;
+	};
+
+
+}
---
+++ src/main/java/com/rcapitol/casamundo/Student.java
@@ -0,0 +1,138 @@
+package com.rcapitol.casamundo;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+
+
+public class Student {
+
+	public Documento documento;
+
+	public Student() {
+
+	}
+
+	public Student(Documento documento) {
+		this.documento = documento;
+	}
+
+	public void setDocumento( Documento documento) {
+		this.documento = documento;
+	}
+
+	public Documento getDocumento() {
+		return this.documento;
+	}
+
+	@Override
+	public String toString() {
+		return new StringBuffer(" Documento : ").append(this.documento).toString();
+	}
+
+	public static final class Documento {
+
+		public String id;
+		public String idDocPrincipal;
+		public String tipo;
+		public String label;
+		
+		public Diagrama diagrama;
+
+
+		public Documento() {
+
+		}
+
+		@JsonCreator
+		public Documento(String id, String idDocPrincipal, String tipo, String label, Diagrama diagrama) {
+			this.id = id;
+			this.idDocPrincipal = idDocPrincipal;
+			this.tipo = tipo;
+			this.label = label;
+			this.diagrama = diagrama;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getId() {
+			return this.id;
+		}
+
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+
+		public String getTipo() {
+			return this.tipo;
+		}
+
+		@Override
+		public String toString() {
+			return new StringBuffer(" Id : ").append(this.id).append(" tipo : ").append(this.tipo).toString();
+		}
+
+
+
+		public static final class Diagrama {
+			public NodeDataArray[] nodeDataArray;
+			public LinkDataArray[] linkDataArray;
+
+			public Diagrama() {
+
+			}
+
+			@JsonCreator
+			public Diagrama(NodeDataArray[] nodeDataArray, LinkDataArray[] linkDataArray) {
+				this.nodeDataArray = nodeDataArray;
+				this.linkDataArray = linkDataArray;
+			}
+
+			public static final class NodeDataArray {
+				public String loc;
+                public String key;
+                public String text;
+                public String color;
+                public String group;
+                public String isGroup;
+                public String id;
+                public String principal;
+       
+
+                public NodeDataArray() {
+
+    			}
+
+                @JsonCreator
+                public NodeDataArray(String loc, String key, String text, String color, String group, String isGroup, String id, String principal){
+                    this.loc = loc;
+                    this.key = key;
+                    this.text = text;
+                    this.color = color;
+                    this.group = group;
+                    this.isGroup = isGroup;
+                    this.id = id;
+                    this.principal = principal;
+                }
+            }
+
+			public static final class LinkDataArray {
+                public String from;
+                public String to;
+                public String color;
+       
+
+                public LinkDataArray() {
+
+    			}
+
+                @JsonCreator
+                public LinkDataArray(String label){
+                    this.from = from;
+                    this.to = to;
+                    this.color = color;
+                }
+            }
+		}
+	}
+}
