21983319ccc9b6a6953c15a291236ad427116cd9
Grenne Reis <grenneglr@gmail.com>
2016-05-31 18:14:01 -0300

    corrigido o erro de nao atualizar os itens das tabelas

--- src/main/webapp/js/administrator.js
+++ src/main/webapp/js/administrator.js
@@ -1,4 +1,36 @@
 
+	/**
+	 * 		setup dos input do form
+	 */
+	var $tablesForm = $("#tables-form").validate({
+		// Rules for form validation
+		rules : {
+		},
+
+		// Messages for form validation
+		messages : {
+		},
+		// form submition
+		submitHandler : function(form) {
+			$.each(form
+			    , function (i, field) {
+					setValueTable (field.id)
+			});
+			rest_atualizaTable(JSON.parse(localStorage.getItem("table")), semAcao, semAcao);
+		},	
+		// Do not change code below
+		errorPlacement : function(error, element) {
+			error.insertAfter(element.parent());
+			$.smallBox({
+				title : "Error",
+				content : "<i class='fa fa-clock-o'></i> <i>There is a error</i>",
+				color : "#ff8080",
+				iconSmall : "fa fa-check fa-2x fadeInRight animated",
+				timeout : 4000
+			});
+		}
+	});
+
 	//
 	//  **** tratar os itens das tabelas
 	//
