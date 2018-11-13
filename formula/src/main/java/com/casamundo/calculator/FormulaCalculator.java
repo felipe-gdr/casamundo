2c9984c2900e960b0cd848b3b0fc5708f914d3b0
Felipe Reis <felipe.gdr@gmail.com>
2018-11-13 11:05:51 +1100

    Adiciona JavaDoc para Formula

--- formula/src/main/java/com/casamundo/calculator/FormulaCalculator.java
+++ formula/src/main/java/com/casamundo/calculator/FormulaCalculator.java
@@ -13,16 +13,99 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Executa o cálculo de uma fórmula levando em conta um mapa de variáveis.
+ * <p>
+ * Exemplo:
+ * <p>
+ * data a fórmula "x + y" e um mapa de variáveis especificando que o valor de "x"
+ * é "1" e o valor de "y" é 2, o resultado do cálculo seria "3".
+ *
+ * <pre>
+ *
+ * final String simpleFormula = "x + y";
+ *
+ * final Map<String, Object> simpleVariables = new HashMap<>();
+ * simpleVariables.put("x", 1);
+ * simpleVariables.put("y", 2);
+ *
+ * final Double result = new FormulaCalculator(simpleFormula, simpleVariables).calculate();
+ *
+ * // valor de "result" é "3.0"
+ * </pre>
+ * <p>
+ * Sintaxes suportadas:
+ *
+ * <p>
+ * <b>operações básicas (adição, multiplicação, divisão, subtração)</b>
+ * <li>"1 + 1" = 2.0</li>
+ * <li>"4 / 2" = 2.0</li>
+ * <li>"2 * 2" = 4.0</li>
+ * <li>"5 - 2" = 3.0</li>
+ * </p>
+ * <p>
+ * <b>ordem de operações, incluindo parentêses</b>
+ * <li>"1 + 2 * 2" = 5.0</li>
+ * <li>"(1 + 2) * 2" = 6.0</li>
+ * </p>
+ * <p>
+ * <b>variáveis</b>
+ * Sempre que variáveis forem utilizadas, é obrigatório passar um {@link Map}
+ * contendo todas as variáveis como chave e seus respectivos valores
+ * <li>"x + y" = 5.0 (dado que o valor de x é 2 e o de y é 3)</li>
+ * </p>
+ * <p>
+ * <b>condicionais</b>
+ * A sintaxe para condicionais é similar à usada no Microsoft Excel:
+ * <pre>
+ *  "if({teste}, {valor-se-verdadeiro}, {valor-se-falso})"
+ *  </pre>
+ * <p>
+ * Também é possível usar "and" em {teste} resultando em regras mais complexas:
+ *
+ * <pre>
+ *  "if(<teste1> and <teste2>, {valor-se-verdadeiro}, {valor-se-falso})"
+ *  </pre>
+ * <p>
+ * Exemplos:
+ * <li>"if(1 = 1, 10, 20)" = 10.0</li>
+ * <li>"if(1 != 1, 10, 20)" = 20.0</li>
+ * <li>"if(x = 1, 10, 20)" = 10.0 (dado que o valor de x é 1)</li>
+ * <li>"if(x = 1, 10, 20)" = 20.0 (dado que o valor de x é diferente de 1)</li>
+ * <li>"if('yes' = 'yes', 10, 20)" = 10.0</li>
+ * <li>"if(yesOrNo = 'yes', 10, 20)" = 10.0 (dado que o valor de yesOrNo é 'yes')</li>
+ * <li>"if(yesOrNo = 'yes', 10, 20)" = 20.0 (dado que o valor de yesOrNo é 'no')</li>
+ * <li>"if(yesOrNo = 'yes' and x = 1, 10, 20)" = 10.0 (dado que o valor de yesOrNo é 'yes' E o valor de x é 1)</li>
+ * <li>"if(yesOrNo = 'yes' and x = 1, 10, 20)" = 10.0 (dado que o valor de yesOrNo é 'yes' OU o valor de x não é 1)</li>
+ * </p>
+ */
 public class FormulaCalculator extends FormulaBaseVisitor<Value> {
     private final String formula;
     private final Map<String, Object> variables;
 
+    /**
+     * Inicia {@link FormulaCalculator}.
+     * <p>
+     * Após criar uma instância de {@link FormulaCalculator}, execute o método
+     * {@link #calculate()} para executar o cálculo e obter um {@link Double}
+     * como resultado.
+     *
+     * @param formula   uma String representando a fórmula a ser calculada
+     *                  ex.: "x + y"
+     * @param variables um mapa contendo os valores das variáveis usadas na fórmula
+     */
     public FormulaCalculator(String formula, Map<String, Object> variables) {
         super();
         this.formula = formula;
         this.variables = variables;
     }
 
+    /**
+     * Executa o cálculo
+     *
+     * @return um {@link Double} contendo o resultado do cálculo.
+     * @throws IOException
+     */
     public Double calculate() throws IOException {
         ByteArrayInputStream byteInputStream = new ByteArrayInputStream(formula.getBytes());
 
--- formula/src/test/java/com/casamundo/calculator/FormulaCalculatorTest.java
+++ formula/src/test/java/com/casamundo/calculator/FormulaCalculatorTest.java
@@ -2,93 +2,24 @@ package com.casamundo.calculator;
 
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class FormulaCalculatorTest {
-    @Parameterized.Parameters(name = "{0} = {1}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                // Arithmetic operations
-                {"1", 1.0, emptyMap()},
-                {"-1", -1.0, emptyMap()},
-                {"1 + 1", 2.0, emptyMap()},
-                {"1 - 1", 0.0, emptyMap()},
-                {"1-1", 0.0, emptyMap()},
-                {"2 * 3", 6.0, emptyMap()},
-                {"4 / 2", 2.0, emptyMap()},
-                {"1 + 2 * 2", 5.0, emptyMap()},
-                {"(1 + 2) * 2", 6.0, emptyMap()},
-                {"2 * 3 * 4", 24.0, emptyMap()},
-                {"10 - 4 / 2", 8.0, emptyMap()},
-                {"(10 - 4) / 2", 3.0, emptyMap()},
-
-                // Variables
-                {"x", 1.0, createVars(var("x", 1.0))},
-                {"x", 0.0, emptyMap()},
-                {"x + y", 10.0, createVars(var("x", 1.0), var("y", 9.0))},
-                {"x / y", 2.0, createVars(var("x", 6.0), var("y", 3.0))},
-                {"-x", -1.0, createVars(var("x", 1.0))},
-                {"-x", 1.0, createVars(var("x", -1.0))},
-
-                // Conditionals
-                {"if(1 = 1, 10, 1)", 10.0, emptyMap()},
-                {"if(1 = 2, 10, 1)", 1.0, emptyMap()},
-                {"if(1 = 2, 10)", 0.0, emptyMap()},
-                {"if(1 != 1, 10, 1)", 1.0, emptyMap()},
-                {"if('yes' = 'yes', 10, 1)", 10.0, emptyMap()},
-                {"if('yes' = 'no', 10, 1)", 1.0, emptyMap()},
-                {"if(2 * 3 = 6, 10, 1)", 10.0, emptyMap()},
-                {"if(2 * 3 = 7, 10, 1)", 1.0, emptyMap()},
-                {"if(x = 1, 10, 1)", 10.0, createVars(var("x", 1.0))},
-                {"if(x != 1, 10, 1)", 1.0, createVars(var("x", 1.0))},
-                {"if(x = 2, x * y, 1)", 6.0, createVars(var("x", 2.0), var("y", 3.0))},
-                {"if(x = 'yes', 1, 0)", 1.0, createVars(var("x", "yes"))},
-                {"if(x = 'yes' and y = 'yes', 1, 0)", 1.0, createVars(var("x", "yes"), var("y", "yes"))},
-                {"if(1 = 1 and 2 = 2, 1, 2)", 1.0, emptyMap()},
-                {"if(1 = 1 and 2 = 3, 1, 2)", 2.0, emptyMap()},
-                {"if(x = 2 and y = 4, x * y, 1)", 1.0, createVars(var("x", 2.0), var("y", 3.0))},
-        });
-    }
-
-    @Parameterized.Parameter
-    public String formula;
-
-    @Parameterized.Parameter(1)
-    public Double expected;
-
-    @Parameterized.Parameter(2)
-    public Map<String, Object> variables;
-
     @Test
-    public void calculate() throws IOException {
-        final Double result = new FormulaCalculator(formula, variables).calculate();
+    public void simpleTest() throws IOException {
+        final String simpleFormula = "x + y";
 
-        assertEquals(expected, result);
+        final Map<String, Object> simpleVariables = new HashMap<>();
+        simpleVariables.put("x", 1);
+        simpleVariables.put("y", 2);
+
+        final Double result = new FormulaCalculator(simpleFormula, simpleVariables).calculate();
+
+        assertEquals(Double.valueOf(3.0), result);
     }
-
-    private static Object[] var(String name, Object value) {
-        return new Object[]{name, value};
-    }
-
-    private static Map<String, Object> createVars(Object[]... vars) {
-        Map<String, Object> result = new HashMap<>();
-
-        Arrays.asList(vars).forEach(var -> {
-            result.put((String) var[0], var[1]);
-        });
-
-        return result;
-    }
-
 }
