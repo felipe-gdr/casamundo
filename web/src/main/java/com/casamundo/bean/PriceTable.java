21bfd6a9c3773953fe5dfc17b60788ede4b35624
Grenne <grenne@glr.com.br>
2018-11-25 10:55:50 -0200

    resolvido o dia a menor nas viagens

--- web/src/main/java/com/casamundo/bean/PriceTable.java
+++ web/src/main/java/com/casamundo/bean/PriceTable.java
@@ -270,7 +270,7 @@ public class PriceTable {
                     resultItem.put("end", end);
                     if (commons.convertDateInt(priceListDoc.getString("to")) < commons.convertDateInt(end)){
                         start = commons.calcNewDate((String) priceListDoc.get("to"), 1);
-                        resultItem.put("end", (String) priceListDoc.get("to"));
+                        resultItem.put("end", commons.calcNewDate((String) priceListDoc.get("to"), 1));
                     }
                     System.out.println("achou preco com vendor entre datas vendor - " + priceListDoc.get("value"));
                     result.add(resultItem);
@@ -356,7 +356,7 @@ public class PriceTable {
                     resultItem.put("end", end);
                     if (commons.convertDateInt(priceListDoc.getString("to")) < commons.convertDateInt(end)){
                         start = commons.calcNewDate((String) priceListDoc.get("to"), 1);
-                        resultItem.put("end", (String) priceListDoc.get("to"));
+                        resultItem.put("end", commons.calcNewDate((String) priceListDoc.get("to"), 1));
                     }
                     System.out.println("achou preco com vendor entre datas vendor - " + priceListDoc.get("value"));
                     result.add(resultItem);
