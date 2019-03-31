872f4cbbbe7bb19ea795d02043ffac41c47f08b5
Felipe Reis <felipe.gdr@gmail.com>
2019-03-31 16:23:59 +1100

    Remove endpoint de login de usuario
    Ja que o Cognito esta se encarregando do login

--- web/src/main/java/com/casamundo/rest/Rest_Usuario.java
+++ web/src/main/java/com/casamundo/rest/Rest_Usuario.java
@@ -1,28 +1,21 @@
 package com.casamundo.rest;
 
-import java.net.UnknownHostException;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.casamundo.dao.Commons_DB;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
 public class Rest_Usuario {
 
-	Commons_DB commons_db = new Commons_DB();
-	
-	@RequestMapping(value = "/obter", produces = "application/json")
-	public BasicDBObject ObterUsuarioName(@RequestParam("email") String email) throws UnknownHostException, MongoException {
-		return commons_db.obterCrudDoc("usuarios", "documento.email", email);
-	};
+    Commons_DB commons_db = new Commons_DB();
 
-	@RequestMapping(value = "/login", produces = "application/json")
-	public BasicDBObject processaLogin(@RequestParam("email") String email, @RequestParam("paswword") String passqord) throws UnknownHostException, MongoException {
-		return commons_db.obterCrudDoc("usuarios", "documento.email", email);
-	};
-};
+    @RequestMapping(value = "/obter", produces = "application/json")
+    public BasicDBObject ObterUsuarioName(@RequestParam("email") String email) throws MongoException {
+        return commons_db.obterCrudDoc("usuarios", "documento.email", email);
+    }
+
+}
